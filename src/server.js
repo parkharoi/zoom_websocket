c1fa0ee203aff29fa4764d1057edfdb35ad4ce2a
parkharoi <haha2000ab@gmail.com>
2022-11-08 01:16:07 +0900

    현재 2번째 파트

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+import { Socket } from "dgram";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.render("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
