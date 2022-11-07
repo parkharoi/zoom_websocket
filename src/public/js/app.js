3fc9db5761a9c60fe769dcdb658997f2fcae1f41
parkharoi <haha2000ab@gmail.com>
2022-11-08 02:03:14 +0900

    feat : 서버와 프론트 양방향 통신
    open, close listener추가함

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+//element를 만들고, socket에 이벤트 open 사용 connection이 open일떄 사용하는 listener 등록
+
+socket.addEventListener("message", (message) => {
+  console.log("New message:", message.data);
+});
+// 서버 연결되어 메세지 받을 때 발생하는 listener
+
+socket.addEventListener("close", () => {
+  console.log("Disconected to Server ⛔️");
+});
+//close 이벤트 서버와 연결이 끊김 = 서버가 오프라인이 됐을 때 사용하는 listener
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -16,9 +16,16 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconected the Browser ⛔️"));
+  socket.on("message", (message) => {
+    console.log(message.toString(`utf8`));
+  });
+  socket.send("hello !!");
+});
+//connection이 생기면 socket을 받는다
+//서버 socket에서 hello 보내줌
+//socket.on("message")는 메세지 이벤트 등록한거임  뒤에 ,message가 메세지 받은거
 
 server.listen(3000, handleListen);
