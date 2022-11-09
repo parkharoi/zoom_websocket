637027143f6a7d3fb3708aea1e0d558381688b14
parkharoi <haha2000ab@gmail.com>
2022-11-09 18:11:50 +0900

    socket IO  기본설정
    socketIO  실행시에
    home.pug의 form을 welcome div에 넣지 않아 app.js가 실행되지 않았으나 해결함

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,55 +1,18 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-  //메세지를 스트링으로 바꿔서 리턴함
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function backendDone(msg) {
+  console.log(`The backend says: `, msg);
 }
 
-function handleOpen() {
-  console.log("Connected to Server ✅");
-}
-
-socket.addEventListener("open", handleOpen);
-//element를 만들고, socket에 이벤트 open 사용 connection이 open일떄 사용하는 listener 등록
-
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-// 서버 연결되어 메세지 받을 때 발생하는 listener 유저가 메시지 전송
-
-socket.addEventListener("close", () => {
-  console.log("Disconected to Server ⛔️");
-});
-//close 이벤트 서버와 연결이 끊김 = 서버가 오프라인이 됐을 때 사용하는 listener
-
-function handleSubmit(event) {
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  //front-end의 form에서 back-end로 무엇가를 보냄
-  const li = document.createElement("li");
-  li.innerText = `You: ${input.value}`;
-  messageList.append(li);
-  input.value = "";
-  // 입력 다 하면 빈값으로 만들어줌
-}
-//이 부분의 메시지는 chat으로 보내는 메시지
-
-function handleNickSubmit(event) {
-  event.preventDefault();
-  //다른 이벤트 실행중일 경우 이벤트 발생 방지
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-  //type은 nickname이고 payloa이고 input.valu이다.
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, backendDone);
+  // backend는 2개의 argument를 받는다. 근데 backendDone이라는 함수는 백에서 모르기 때문에 보안 문제로 프론트에서 실행함
   input.value = "";
 }
-//이 부분의 메시지는 내가 nickname을 변경하고 싶을 때 back-end로 보내는거
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
 import express from "express";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -10,46 +10,51 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.render("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+// 먼저 http 서버를 만들었지
+const wsServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  //누군가 우리 서버에 연결하면, 그 connection을 여기에 넣을거야
-  // 이 뜻은 firefox가 연결될 떄, firefox를 이 array에 넣어 준다.
-
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  //닉네임을 지정하지 않은 사람들을 위한 익명 배려임
-
-  console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconected the Browser ⛔️"));
-  socket.on("message", (msg) => {
-    //sovket이 메시지를 보낼 때까지 기다릴꼬얌 (msg) string을 받고
-
-    const message = JSON.parse(msg);
-    //string을 parse해서 massage로 바꿨어
-
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${message.payload}`)
-        );
-      //모두에게 익명의 socket이 보낸 메시지를 전달 하는거여 nickname property(익명)를 socket object에 저장하는겨
-      //이렇게 하는이유는 닉네임만 입력하고 버튼을 눌렀을경우 전송하는 것을 방지해준다.
-
-      case "nickname":
-        socket["nickname"] = message.payload;
-      //나중에 nickname ype의 메시지를 전송할 수도 있겠지?
-    }
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event:${event}`);
   });
-  // 각 브라우저를 aSocket으로 표시하고 메시지를 보낸다는 의미 이제 연결된 모든 소켓 연결
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    // socketIO에서 모든 socket은 기본적으로 User와 서버 사이에 private romm이 있다.
+    //이게 방에 참가하는 기능임
+    setTimeout(() => {
+      done("hello from the backend");
+    }, 15000);
+    //백에서 함수 done 함수 실행시킴
+  });
 });
-//connection이 생기면 socket을 받는다
-//서버 socket에서 hello 보내줌
-//socket.on("message")는 메세지 이벤트 등록한거임  뒤에 ,message가 메세지 받은거
+//conection을 받을 준비
 
-server.listen(3000, handleListen);
+// const wss = new WebSocket.Server({ server });
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon";
+
+//   console.log("Connected to Browser ✅");
+//   socket.on("close", () => console.log("Disconected the Browser ⛔️"));
+//   socket.on("message", (msg) => {
+
+//     const message = JSON.parse(msg);
+
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname} : ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
